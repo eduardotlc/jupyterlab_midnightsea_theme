3f714b323e9cbdb8277b9b5c628f80aac70f9d87
Eduardo Campos <eduardotcampos@usp.br>
2023-07-19 19:44:58 -0300

    correct index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,18 @@ import { IThemeManager } from '@jupyterlab/apputils';
  * Initialization data for the jupyterlab_ariakedark_theme extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab_materialdarker_theme:plugin',
-  description: 'A material darker Jupyterlab theme extension.',
+  id: 'jupyterlab_midnightsea_theme:plugin',
+  description: 'A dark blue and purple Jupyterlab theme extension.',
   autoStart: true,
   requires: [IThemeManager],
   activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
     console.log(
-      'JupyterLab extension jupyterlab_materialdarker_theme is activated!'
+      'JupyterLab extension jupyterlab_midnightsea_theme is activated!'
     );
-    const style = 'jupyterlab_materialdarker_theme/index.css';
+    const style = 'jupyterlab_midnightsea_theme/index.css';
 
     manager.register({
-      name: 'jupyterlab_materialdarker_theme',
+      name: 'jupyterlab_midnightsea_theme',
       isLight: false,
       load: () => manager.loadCSS(style),
       unload: () => Promise.resolve(undefined)
